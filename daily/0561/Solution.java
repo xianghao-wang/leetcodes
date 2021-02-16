ebde70425cfbe4a6c481e4973435663c28f4d637
xianghao <xianghaowang2001@gmail.com>
2021-02-16 10:31:22 +0800

    daily problem of 561

---
+++ daily/0561/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int ans = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            // ans += min(nums[i], nums[i + 1]);
+            ans += nums[i];
+        }
+
+        return ans;
+    }
+}
