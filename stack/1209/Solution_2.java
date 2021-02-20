7a5b800a5a2baa22190f1c60b9a38d33fad5a165
xianghao <xianghaowang2001@gmail.com>
2021-02-20 22:09:25 +0800

    add solution2 of 1209 to stack

--- stack/1209/Solution_2.java
+++ stack/1209/Solution_2.java
@@ -1,6 +1,6 @@
 import java.util.Stack;
 
-class Solution_2 {
+class Solution {
     public String removeDuplicates(String s, int k) {
         int pAns = 0, p = 0;
         char[] ans = s.toCharArray();
@@ -9,17 +9,21 @@ class Solution_2 {
         while (p < ans.length) {
             ans[pAns] = ans[p];
 
-            if (j == 0 || ans[j] != ans[j - 1]) {
+            if (pAns == 0 || ans[pAns] != ans[pAns - 1]) {
                 counts.push(1);
             } else {
                 int n = counts.pop() + 1;
                 if (n == k) {
-                    j -= k;
+                    pAns -= k;
+                } else {
+                    counts.push(n);
                 }
             }
 
             p ++;
             pAns ++;
         }
+
+        return new String(ans, 0, pAns);
     }
 }
